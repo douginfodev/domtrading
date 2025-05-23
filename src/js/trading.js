37ef67b2fe6cf1c52ed5c4981046cd831671982a
= <=>
2025-05-23 06:51:04 -0300

    feat: Created trading.js file

---
+++ src/js/trading.js
@@ -0,0 +1,24 @@
+//Trading - Constructor
+function Trading(id, startprice, endprice, actualprice) {
+
+    //CANDLE PROPERTIES
+    this.Id = id,
+    this.startPrice = startprice,
+    this.endPrice = endprice,
+    this.actualPrice = actualprice
+};
+
+//Price Value Direction
+Trading.prototype.priceAction = function (actualPrice) {
+    let priceActionDirection = 'INITIAL';
+
+    if (this.startPrice != actualPrice) {
+        priceActionDirection = 'UP';
+
+        if (actualPrice < this.startPrice)
+            priceActionDirection = 'DOWN';
+
+    }
+
+    return priceActionDirection;
+}
